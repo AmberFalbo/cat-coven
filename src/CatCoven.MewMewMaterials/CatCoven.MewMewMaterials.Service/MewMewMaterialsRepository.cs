3f4c23553136fa609301f9d3d1edf25e4b4811bf
Scott Falbo <scottfalboart@gmail.com>
2023-11-06 18:23:56 -0800

    started implementing repo layer

--- src/CatCoven.MewMewMaterials/CatCoven.MewMewMaterials.Service/MewMewMaterialsRepository.cs
+++ src/CatCoven.MewMewMaterials/CatCoven.MewMewMaterials.Service/MewMewMaterialsRepository.cs
@@ -2,33 +2,82 @@
 // Cat Coven API
 // ------------------------------------
 
+using CatCoven.MewMewMaterials.Data;
 using CatCoven.MewMewMaterials.Service.Models;
 
 namespace CatCoven.MewMewMaterials.Service
 {
     public class MewMewMaterialsRepository : IMewMewMaterialsRepository
     {
+        private readonly MaterialsDbContext _context;
         private readonly ILogger _logger;
 
-        public MewMewMaterialsRepository(
-            ILogger<MewMewMaterialsRepository> logger)
+        public MewMewMaterialsRepository(MaterialsDbContext context, ILogger<MewMewMaterialsRepository> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public Task<Cache> CreateCache(Cache cache)
+        public async Task<Cache> CreateCache(Cache cache)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Caches.AddAsync(cache);
+                await _context.SaveChangesAsync();
+                return cache;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a cache.");
+                throw;
+            }
         }
 
-        public Task<Cache> GetCache(string cacheId)
+        public async Task<Cache> GetCache(string cacheId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var cache = await _context.Caches.FindAsync(Guid.Parse(cacheId));
+                return cache;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while retrieving a cache.");
+                throw;
+            }
         }
 
-        public Task<Cache> UpdateCache(Cache cache)
+        public async Task<Cache> UpdateCache(Cache cache)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Caches.Update(cache);
+                await _context.SaveChangesAsync();
+                return cache;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while updating a cache.");
+                throw;
+            }
+        }
+
+        public async Task DeleteCache(string cacheId)
+        {
+            try
+            {
+                var cache = await _context.Caches.FindAsync(Guid.Parse(cacheId));
+                if (cache != null)
+                {
+                    _context.Caches.Remove(cache);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while deleting a cache.");
+                throw;
+            }
         }
     }
 }
